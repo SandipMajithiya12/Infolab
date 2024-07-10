f342ed9cc78ab78b376b0d8587b4a1fd3c7bbca9
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2024-07-10 22:31:15 +0530

    task;

---
+++ 3_july/3rd/src/Button.jsx
@@ -0,0 +1,18 @@
+// src/Buttons.js
+
+import Login, { Addtocart, Logout } from "./cumponents";
+
+
+
+const Buttons = () => {
+  return (
+    <div>
+      <h2>Buttons Component</h2>
+      <Login />
+      <Logout />
+      <Addtocart />
+    </div>
+  );
+};
+
+export default Buttons;
